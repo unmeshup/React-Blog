a607b1f7d0e16d3001ba51a788382e48acda579d
Unmesh Pawar <unplfc@gmail.com>
2023-04-07 13:38:58 +0530

    done

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,7 +1,23 @@
 import { firestore } from '../firebase';
 import { useFormInput } from '../hooks';
+import styled, { css } from 'styled-components';
+// import classes from './Button.module.css';
 
-import classes from './Button.module.css';
+const StyledButton = styled.button`
+  height: 33px;
+  background: ${(props) => (props.primary ? '#4caf50' : 'blue')};
+  border: 0;
+  color: #fff;
+  padding: 8px;
+  font-size: 15px;
+  border-radius: 3px;
+  cursor: pointer;
+  ${(props) =>
+    props.primary &&
+    css`
+      border: 4px solid ${props.bgColor};
+    `};
+`;
 
 function CreatePost() {
   const title = useFormInput('');
@@ -43,7 +59,10 @@ function CreatePost() {
           <textarea {...content}></textarea>
         </div>
 
-        <button className={classes.createPostBtn}>Create Post</button>
+        <StyledButton primary bgColor="blue">
+          Create Post
+        </StyledButton>
+        {/* <StyledButton primary>Create Post</StyledButton> */}
       </form>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -13,6 +13,33 @@ const PostSubTitle = styled.p`
   font-size: 13px;
 `;
 
+const Post = styled.div`
+  border: 1px solid #e1e1e1;
+  padding: 10px 10px;
+  border-radius: 5px;
+  margin-top: 10px;
+
+  &:hover {
+    border: 1px solid #2196f3;
+  }
+
+  h3 {
+    margin: 0;
+    padding: 0;
+    font-size: 25px;
+    font-weight: bold;
+    color: black;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  @media (max-width: 800px) {
+    border: 1px solid black;
+  }
+`;
+
 function Home() {
   const [posts, setPosts] = useState([]);
 
@@ -40,13 +67,13 @@ function Home() {
       <div id="blog-by">Aakash</div>
 
       {posts.map((post, index) => (
-        <div className="post" key={`post-${index}`}>
+        <Post className="post" key={`post-${index}`}>
           <Link to={`/post/${post.id}`}>
             <h3>{post.title}</h3>
           </Link>
 
           <PostSubTitle>{post.subTitle}</PostSubTitle>
-        </div>
+        </Post>
       ))}
     </div>
   );
